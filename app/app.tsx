c0d7b551bc61df1072a583eea236c94c4841e716
remuria <aaa@aaa.com>
2021-10-21 14:06:55 +0900

    TypeScript導入完了

---
+++ app/app.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import Sample from "./script";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Sample></Sample>
+  </React.StrictMode>,
+  document.getElementById("app")
+);
--- app/script.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-
-const Hello = () => <h1>どうも🎵</h1>;
-export default Hello;
